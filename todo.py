e654c9fed0583e653e5434b209ee5cd4d8f17502
Usman Hussain <askUsmanHussain@gmail.com>
2025-02-26 18:40:04 +0500

    Add simple command-line To-Do List application with basic task management features

---
+++ todo.py
@@ -0,0 +1,61 @@
+def display_menu():
+    print('\n Menu: ')
+    print('1. Add task')
+    print('2. View tasks')
+    print('3. Mark as done')
+    print('4. Exit')
+
+def add_task(tasks):
+    task = str(input("Please input task description: "))
+    tasks.append(task)
+    print('Task Added Successfully')
+
+def view_tasks(tasks):
+    print('\n Tasks:')
+    [print(f"{index}. {x}") for index, x in enumerate(tasks)]
+
+def tasks_mark_done(tasks):
+    if not tasks: # if len(tasks) == 0:
+        print("No tasks to mark as done.")
+        return
+    
+    view_tasks(tasks)
+    index = int(input("Enter task index to mark as done: "))
+
+    try:
+        print(f"Item at index {index}: {tasks[index]}")
+        removed_tasks = tasks.pop(index)
+        print(f"{removed_tasks} has been removed and marked as done.")
+    except IndexError:
+        print("Index out of range or Invalid index.")
+
+
+def main():
+
+    tasks = []
+
+    while True:
+        display_menu()
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_task(tasks)
+        elif choice == '2':
+            view_tasks(tasks)
+        elif choice == '3':
+            tasks_mark_done(tasks)
+        elif choice == '4':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+if __name__ == "__main__":
+    main()
+
+
+    
+
+
+
