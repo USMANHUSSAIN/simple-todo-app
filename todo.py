9b1019fbc59c3483b62e97fb4f4b163c3fd8f8cf
Usman Hussain <AskUsmanHussain@gmail.com>
2025-02-26 18:57:30 +0500

    Add task persistence with file storage; Formatting the code via pyLint

--- todo.py
+++ todo.py
@@ -5,20 +5,23 @@ def display_menu():
     print('3. Mark as done')
     print('4. Exit')
 
+
 def add_task(tasks):
     task = str(input("Please input task description: "))
     tasks.append(task)
     print('Task Added Successfully')
 
+
 def view_tasks(tasks):
     print('\n Tasks:')
     [print(f"{index}. {x}") for index, x in enumerate(tasks)]
 
+
 def tasks_mark_done(tasks):
-    if not tasks: # if len(tasks) == 0:
+    if not tasks:
         print("No tasks to mark as done.")
         return
-    
+
     view_tasks(tasks)
     index = int(input("Enter task index to mark as done: "))
 
@@ -30,9 +33,23 @@ def tasks_mark_done(tasks):
         print("Index out of range or Invalid index.")
 
 
+def save_tasks(tasks):
+    with open("tasks.txt", "w") as f:
+        for task in tasks:
+            f.write(task + '\n')
+
+
+def load_tasks():
+    try:
+        with open("tasks.txt", "r") as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        return []
+
+
 def main():
 
-    tasks = []
+    tasks = load_tasks()
 
     while True:
         display_menu()
@@ -47,15 +64,11 @@ def main():
             tasks_mark_done(tasks)
         elif choice == '4':
             print("Exiting...")
+            save_tasks(tasks)
             break
         else:
             print("Invalid choice. Please select a valid option.")
 
+
 if __name__ == "__main__":
     main()
-
-
-    
-
-
-
